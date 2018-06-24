2be461fb03257e63703a43aff2d2b55481177559
mduske <mark@duske.pw>
2018-06-24 19:25:19 -0400

    d

--- src/colonyClient.js
+++ src/colonyClient.js
@@ -26,7 +26,7 @@ let knownColoniesCache = [];
  * @param {*} _tokenSymbol 
  */
 exports.createColony = async function (_accountNumber, _tokenName, _tokenSymbol) {
-    console.log("createColonyClient");
+    console.log("createColony");
 
     // Get the private key from the selected account from the ganache-accounts
     // through trufflepig
@@ -66,18 +66,55 @@ exports.createColony = async function (_accountNumber, _tokenName, _tokenSymbol)
     // For a colony that exists already, you just need its ID:
     const colonyClient = await networkClient.getColonyClient(colonyId);
 
-    return colonyClient;
+    const colonyData = await exports.gatherColonyData(colonyClient, colonyId);
+
+    return {"colonyClient": colonyClient, "colonyData": colonyData};
+};
+
+exports.openColony = async function (_accountNumber, _colonyId, _colonyAddress) {
+    console.log("openColony " +_accountNumber + ", " + _colonyId + ", " + _colonyAddress);
+
+    // Get the private key from the selected account from the ganache-accounts
+    // through trufflepig
+    const { privateKey } = await loader.getAccount(_accountNumber);
+
+    // Create a wallet with the private key (so we have a balance we can use)
+    const wallet = new Wallet(privateKey, provider);
+
+    // Create an adapter (powered by ethers)
+    const adapter = new EthersAdapter({
+        loader,
+        provider,
+        wallet,
+    });
+
+    // Connect to ColonyNetwork with the adapter!
+    const networkClient = new ColonyNetworkClient({ adapter });
+    await networkClient.init();
+
+    console.log("openColony2 " +_accountNumber + ", " + _colonyId + ", " + _colonyAddress);
+
+    // For a colony that exists already, you just need its ID:
+    //const colonyClient = await networkClient.getColonyClient(_colonyId);
+    const colonyClient = await networkClient.getColonyClientByAddress(_colonyAddress);
+
+    console.log("openColony3 " +_accountNumber + ", " + _colonyId + ", " + _colonyAddress);
+    const colonyData = await exports.gatherColonyData(colonyClient, _colonyId);
+
+    console.log("openColony4 " +_accountNumber + ", " + _colonyId + ", " + _colonyAddress);
+    return {"colonyClient": colonyClient, "colonyData": colonyData};
 };
 
 /**
  * List all known colonies
+ * TODO: Process Colonies in parallel
  * @param {*} _accountNumber 
  */
 exports.listColonies = async function (_accountNumber) {
     console.log("listColonies " + _accountNumber);
-    let colonies = [];
+    const colonies = [];
 
-    if (knownColoniesCache.length>0) {
+    if (knownColoniesCache.length > 0) {
         console.log("reading from Cache with " + knownColoniesCache.length + " entries.");
         tempColonyList = [];
 
@@ -111,27 +148,19 @@ exports.listColonies = async function (_accountNumber) {
 
     //List Colonies
     try {
+        const colonyClientPromises = [];
         const totalColonyCount = (await networkClient.getColonyCount.call()).count;
         for (x = 1; x <= totalColonyCount; x++) {
             console.log("Extracting Colony Data " + x);
             const colonyClient = await networkClient.getColonyClient(x);
 
-            const colonyTokenInfo = await colonyClient.token.getTokenInfo.call();
-            const colonyDomainCount = (await colonyClient.getDomainCount.call()).count;
-            const colonyTaskCount = (await colonyClient.getTaskCount.call()).count;
-            const colonyContractAddress = colonyClient.contract.address;
+            colonyData = await exports.gatherColonyData(colonyClient, x);
 
-            //Gather data
-            const colonyData = {
-                "token": colonyTokenInfo,
-                "domainCount": colonyDomainCount,
-                "taskCount": colonyDomainCount,
-                "address": colonyContractAddress,
-                "id": x
-            };
             console.log(colonyData);
 
             colonies.push(colonyData);
+
+            //break; //REMOVE
         }
     } catch (e) {
         console.log(e);
@@ -139,6 +168,30 @@ exports.listColonies = async function (_accountNumber) {
 
     return colonies;
 };
+
+/**
+ * Gathers simply and consistent represenation of the Colony
+ * @param {*} _colonyClient
+ */
+exports.gatherColonyData = async function (_colonyClient, _id) {
+    console.log("gatherColonyData " + _id);
+    const colonyTokenInfo = await _colonyClient.token.getTokenInfo.call();
+    const colonyDomainCount = (await _colonyClient.getDomainCount.call()).count;
+    const colonyTaskCount = (await _colonyClient.getTaskCount.call()).count;
+    const colonyContractAddress = _colonyClient.contract.address;
+    const colonyId = _id;
+
+    //Gather data
+    const colonyData = {
+        "token": colonyTokenInfo,
+        "domainCount": colonyDomainCount,
+        "taskCount": colonyDomainCount,
+        "address": colonyContractAddress,
+        "id": colonyId
+    };
+
+    return colonyData;
+}
 
 /**
  * List Existing Accounts
